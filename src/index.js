46a92326c9bb9fd9aa54f5373d0b29be1b604d10
Your Name <you@example.com>
2020-10-12 23:21:34 +0300

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const arrSymbols = expr.match(/.{1,10}/g);
+    let decodeWord = "";
+
+    arrSymbols.forEach(function(symbol) {
+        const arrSymbolCodes = symbol.match(/.{1,2}/g);
+        let strSymbol = "";
+
+        if (arrSymbolCodes[0] === "**") {
+            decodeWord += " ";
+        } else {
+            arrSymbolCodes.forEach(function(code) {
+                switch (code) {
+                    case "00":
+                        return;
+                    case "10":
+                        strSymbol += ".";
+                        break;
+                    case "11":
+                        strSymbol += "-";
+                        break;
+                }
+            });
+            decodeWord += MORSE_TABLE[strSymbol];
+            strSymbol = "";
+        }
+    });
+    return decodeWord;
 }
 
 module.exports = {
